c090c3620f80d6daf6dd851cac95f823050711e6
Joakim <joakim@spprod.se>
2023-03-01 17:01:59 +0100

    Update category button UI to respond correctly

---
+++ pages/api/login.js
@@ -0,0 +1,5 @@
+
+
+export default function handle(req, res) {
+    res.json(200, {message: "You've logged in"})
+}
--- pages/login/Index.js
+++ pages/login/Index.js
@@ -1,6 +1,22 @@
 import Link from "next/link"
+import { useRef } from "react"
+import prisma from "../../lib/prisma"
+
 
 export default function Index() {
+    const userName = useRef()
+    const passWord = useRef()
+
+    function checkLoginInfo(e) {
+        e.preventDefault()
+        
+        try {
+            const respons = fetch('/../')
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
         <div className="h-screen flex justify-center items-center">
             <div className="flex-1 grid grid-cols-2 justify-items-center items-center border-8 rounded-xl h-4/5 sm:mx-32 p-20 ">
@@ -11,14 +27,24 @@ export default function Index() {
 
                     <img src="http://placekitten.com/150/150" className="rounded-full m-auto border-4 border-black"></img>
                     <h1 className="text-5xl mb-14 mt-8 text-center">WELCOME</h1>
-                    <form action="" method="POST">
+                    <form onSubmit={checkLoginInfo}>
                         <div className="mb-20 ">
                             <div><label htmlFor="username">Username</label></div>
-                            <input type="text" id="username" name="username" className="border-b-4 w-96 mt-4 outline-none bg-transparent"></input>
+                            <input
+                                type="text"
+                                id="username"
+                                name="username"
+                                ref={userName}
+                                className="border-b-4 w-96 mt-4 outline-none bg-transparent" />
                         </div>
                         <div className="mb-2">
                             <div><label htmlFor="password">Password</label></div>
-                            <input type="password" id="password" name="password" className="border-b-4 w-96 mt-4 outline-none bg-transparent"></input>
+                            <input
+                                type="password"
+                                id="password"
+                                name="password"
+                                ref={passWord}
+                                className="border-b-4 w-96 mt-4 outline-none bg-transparent" />
                             <p className="text-right"><a href="#">Forgot password?</a></p>
                         </div>
                         <div>

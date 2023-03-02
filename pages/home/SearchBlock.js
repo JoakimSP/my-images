8f626d6c667bb8e943900105fd7af95138bad74a
Joakim <joakim@spprod.se>
2023-03-02 08:30:16 +0100

    Change categories variable to state, save queryinput in useRef

--- pages/home/SearchBlock.js
+++ pages/home/SearchBlock.js
@@ -1,26 +1,23 @@
 import BgImage from '../../public/5EC255C4-E950-4A1A-B1AE-2388483BA5C5.JPG'
-import { useState, useEffect, useRef } from 'react'
+import { useState, useRef } from 'react'
 
 export default function SearchBlock() {
   const [toggleClassName, setClassName] = useState(false)
-  const catrefs = useRef()
-  const [cat, setCat] = useState({
-    all: "All Categories",
-    Nature: "Nature",
-    Humans: "Humans",
-    Culture: "Culture",
-    Animals: "animals"
-  })
+  const query = useRef()
+  const [cat, setCat] = useState("All Categories")
+
 
 
 
 
   function updateCategory(event) {
     const text = event.target.textContent
-    
-    const newText = cat.find(prop => prop === text)
-    console.log(newText)
+    setCat(text)
+  }
 
+  function displayInput(e){
+    query.current = e.target.value
+    console.log(query)
   }
 
 
@@ -28,10 +25,10 @@ export default function SearchBlock() {
     <div className="flex place-items-center justify-center h-96 bg-[length:1920px_600px] bg-no-repeat px-7 sm:w-screen" style={{ backgroundImage: `url(${BgImage.src})` }} >
       <div className="flex">
         <label htmlFor="search-dropdown" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Your Email</label>
-        <button onMouseEnter={() => setClassName(true)} id="dropdown-button" data-dropdown-toggle="dropdown" className="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600" type="button">{cat.all} <svg aria-hidden="true" className="w-4 h-4 ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"></path></svg></button>
+        <button onMouseEnter={() => setClassName(true)} id="dropdown-button" data-dropdown-toggle="dropdown" className="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600" type="button">{cat} <svg aria-hidden="true" className="w-4 h-4 ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"></path></svg></button>
 
         <div className="relative w-full">
-          <input type="search" id="search-dropdown" className="block p-2.5 w-96 z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" placeholder="Search Mockups, Logos, Design Templates..." required />
+          <input onChange={displayInput} type="search" id="search-dropdown" className="block p-2.5 w-96 z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" placeholder="Search Mockups, Logos, Design Templates..." required />
           <button type="submit" className="absolute top-0 right-0 p-2.5 text-sm font-medium text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             <svg aria-hidden="true" className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
             <span className="sr-only">Search</span>
@@ -40,6 +37,9 @@ export default function SearchBlock() {
 
         <div id="dropdown" onMouseLeave={() => setClassName(false)} className={`z-10 absolute top-72 ${toggleClassName ? 'block' : 'hidden'} bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700`}>
           <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdown-button">
+          <li>
+              <button onClick={updateCategory}  type="button" className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">All Categories</button>
+            </li>
             <li>
               <button onClick={updateCategory}  type="button" className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Nature</button>
             </li>
